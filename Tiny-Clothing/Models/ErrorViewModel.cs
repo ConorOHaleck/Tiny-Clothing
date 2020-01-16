4ca2673ad4d0186b70c54ca1c501eeda2dc08f6c
Conor O'Haleck <conorohaleck@gmail.com>
2020-01-16 09:16:46 -0800

    Add project files.

---
+++ Tiny-Clothing/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tiny_Clothing.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
