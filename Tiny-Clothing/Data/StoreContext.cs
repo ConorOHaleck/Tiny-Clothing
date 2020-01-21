5cc684e469e6700df23167cca864f45a1bb7e0b4
Conor O'Haleck <conorohaleck@gmail.com>
2020-01-21 10:35:20 -0800

    Added DbContext and StoreContext class to utilize it

---
+++ Tiny-Clothing/Data/StoreContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tiny_Clothing.Models;
+
+namespace Tiny_Clothing.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+
+        }
+            //Add a DbSet for each Entity that needs a table in the database
+            public DbSet<Item> Items { get; set; }
+    }
+    }
+}
